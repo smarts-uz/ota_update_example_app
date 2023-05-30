a921a5a1a599b78a521371ed38cb01b1c601fe29
Muhammad Azizullo <gulamovmazizullo@gmail.com>
2023-05-30 11:47:46 +0500

    first
    first

---
+++ android/app/src/main/kotlin/com/azizullo/auto_update_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.azizullo.auto_update_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:ota_update/ota_update.dart';
+
+void main() => runApp(const MyApp());
+
+/// example widget for ota_update plugin
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  OtaEvent? currentEvent;
+
+  @override
+  void initState() {
+    super.initState();
+    tryOtaUpdate();
+  }
+
+  Future<void> tryOtaUpdate() async {
+    try {
+      print('ABI Platform: ${await OtaUpdate().getAbi()}');
+      //LINK CONTAINS APK OF FLUTTER HELLO WORLD FROM FLUTTER SDK EXAMPLES
+      OtaUpdate()
+          .execute(
+        'https://internal1.4q.sk/flutter_hello_world.apk',
+        destinationFilename: 'flutter_hello_world.apk',
+        //FOR NOW ANDROID ONLY - ABILITY TO VALIDATE CHECKSUM OF FILE:
+        sha256checksum: 'd6da28451a1e15cf7a75f2c3f151befad3b80ad0bb232ab15c20897e54f21478',
+      )
+          .listen(
+            (OtaEvent event) {
+          setState(() => currentEvent = event);
+        },
+      );
+      // ignore: avoid_catches_without_on_clauses
+    } catch (e) {
+      print('Failed to make OTA update. Details: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (currentEvent == null) {
+      return Container();
+    }
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: Center(
+          child: Text('OTA status: ${currentEvent!.status} : ${currentEvent!.value} \n'),
+        ),
+      ),
+    );
+  }
+}
